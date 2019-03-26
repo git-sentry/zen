5f23c2a8186657c4ab8d887f761cc59a10cfc2a7
Dragos Dumitrache <dragosd2000@gmail.com>
2019-03-26 00:10:12 +0000

    Add initial login to git

--- zen/main/zen.py
+++ zen/main/zen.py
@@ -1,7 +1,12 @@
 #/usr/local/bin/python3
 
+import os
+
+from github3 import login
+
 def main():
-    print('Hello World, I am zen!')
+    git = login(token=os.environ['ZEN'])
+    print(f'Hello, I am {git.me()}')
 
 if __name__ == '__main__':
     main()
