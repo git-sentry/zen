3eadaf63370231c814fc5ae4ee3661e60db56ce1
Dragos Dumitrache <dragosd2000@gmail.com>
2019-03-26 00:23:44 +0000

    Adding click and initial command
    Prints user repositories, but is not parameterized

--- zen/main/zen.py
+++ zen/main/zen.py
@@ -1,12 +1,28 @@
 #/usr/local/bin/python3
 
 import os
+import click
 
 from github3 import login
 
-def main():
+
+@click.group()
+def zen_cli():
+    pass
+
+
+@zen_cli.command()
+def repos():
     git = login(token=os.environ['ZEN'])
-    print(f'Hello, I am {git.me()}')
+
+    click.echo(f'{git.me().login} owns the following repositories:')
+    for repository in git.repositories_by(git.me().login):
+        click.echo(f'{repository.name}')
+
+
+def main():
+    zen_cli()
+
 
 if __name__ == '__main__':
     main()
