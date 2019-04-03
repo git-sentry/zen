ed5679400264f889be8c7c57dea9f62dc8586c0e
Dragos Dumitrache <dragosd2000@gmail.com>
2019-04-04 00:20:36 +0100

    Finished filtering repos. Added clone command. TODO: Improve testability and coverage

--- zen/main/zen.py
+++ zen/main/zen.py
@@ -1,33 +1,23 @@
-#/usr/local/bin/python3
+# /usr/local/bin/python3
 
 import os
 import click
 import re
+import subprocess
 
 from github3 import login
 from click import pass_context
-from functools import update_wrapper
 
-@click.group(chain=True)
-def zen_cli():
+
+@click.group()
+@pass_context
+def zen_cli(ctx):
     pass
 
-
-
-@zen_cli.command()
-@click.argument('username', default=None, required=False)
-@click.option('-p', '--patterns',  default=['.*'], multiple=True)
-@pass_context
-def clone(ctx, username, patterns):
-
-    repositories = ctx.forward(repos)
-
-    for repo in repositories:
-        click.echo(f'Cloning {repo.name}')
-
-    return repositories
-
-
+# TODO: Add config.ini support later on
+# TODO: Add workspace env variable for cloning
+# TODO: Improve testability.
+# TODO: Consider Cucumber tests for python
 @zen_cli.command()
 @click.argument('username', default=None, required=False)
 @click.option('-p', '--patterns', multiple=True, default=['.*'])
@@ -44,7 +34,7 @@ def repos(ctx, username, patterns):
     filter_by = '|'.join([r'(\S*{}\S*)'.format(p) for p in patterns])
     regex = re.compile(filter_by)
 
-    repositories = [r for r in repositories if regex.search(r.name)]
+    repositories = [r for r in repositories if regex.findall(r.name)]
 
     if ctx.parent.parent is None:
         click.echo(f'{username} has access to the following repositories:')
@@ -53,6 +43,30 @@ def repos(ctx, username, patterns):
             click.echo(f'{repository.name}')
 
     return repositories
+
+
+@zen_cli.command()
+@click.argument('username', default=None, required=False)
+@click.option('-p', '--patterns',  default=['.*'], multiple=True)
+@pass_context
+def clone(ctx, username, patterns):
+
+    repositories = ctx.forward(repos)
+
+    for repo in repositories:
+        _repository_clone(repo)
+
+    return repositories
+
+
+def _repository_clone(repository, path=os.environ['WORKSPACE']):
+    repo_dir = os.path.join(path, repository.name)
+    if os.path.isdir(repo_dir):
+        click.echo(f'{repo_dir} already exists')
+        return False
+
+    subprocess.run(['git', 'clone', repository.ssh_url, repo_dir])
+    return True
 
 
 def main():
